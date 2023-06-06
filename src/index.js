777cf9bfca6bc22f5bbad1716ad1f32d85153b56
louadjRania <r.louadj@esi-sba.dz>
2023-06-06 18:33:08 +0100

    name

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Khidma from './App'; // Remplacez "App" par le nom de votre composant principal
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Khidma /> {/* Remplacez "App" par le nom de votre composant principal */}
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
